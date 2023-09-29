3d2fccc2c1195b8b2b5922dc21649d95d4bd37fd
nikeshskillup <nikesh_kr@skillup.online>
2023-09-29 12:14:10 +0530

    message

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,21 +8,12 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
-    # path for about view
-
-    # path for contact us view
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
     path(route='', view=views.get_dealerships, name='index'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
+    path('dealer/<int:id>/review', views.add_review, name='add_review'),
+    path(route='about/', view=views.about, name='about'),
+    path(route='contact/', view=views.contact, name='contact'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,8 +27,10 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ['localhost', 'localhost:8000', 'cardealers210922.eu-gb.mybluemix.net', 'https://ksundararaja-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
+
+CSRF_TRUSTED_ORIGINS = ['https://ksundararaja-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 # Application definition
 
